0c604d6ca3d0f3f953781bf7e434f9ee5e14d328
Siak Hooi <siakhooi@gmail.com>
2023-05-02 18:26:56 +0800

    fix name issue

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,12 +3,19 @@
 EXPECTED_DIRECTORY=test/expected
 ACTUAL_DIRECTORY=$(mktemp -d)
 
+if [[ $# -eq 1 ]] && [[ -d "$1" ]]; then
+  ACTUAL_DIRECTORY=$1
+fi
+
 test/generate-test-output.sh "$ACTUAL_DIRECTORY"
 
-diff $EXPECTED_DIRECTORY $ACTUAL_DIRECTORY 
+diff $EXPECTED_DIRECTORY $ACTUAL_DIRECTORY
 
-if [ $? -eq 0 ] ; then echo.green "Look good!"; else echo.red "Something wrong!" ; fi
-
+RESULT=$?
 echo.blue "Actual Result in: $ACTUAL_DIRECTORY"
 
 ls -l "$ACTUAL_DIRECTORY"
+
+if [ $RESULT -eq 0 ]; then echo.green "Look good!"; else echo.red "Something wrong!"; fi
+
+exit $RESULT
