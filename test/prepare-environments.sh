3bc8e9ea294715ad5b86e3aac99c05744499c257
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2023-05-10 10:02:37 +0000

    refactor test configs

--- test/prepare-environments.sh
+++ test/prepare-environments.sh
@@ -4,10 +4,10 @@ DEBFILE=$(find . -name '*.deb')
 apt install -y "$DEBFILE"
 echo 'source /usr/lib/shed/shed-init' >>~/.bashrc
 
-# test related
+# prepare configs
 mkdir -p ~/.shed/kubectl-config
-cp -v test/kubeconfig-D ~/.shed/kubectl-config
-cp -v test/kubeconfig-T ~/.shed/kubectl-config
-## test related end
+cp -v test/test-configs/kubeconfig-D ~/.shed/kubectl-config
+cp -v test/test-configs/kubeconfig-T ~/.shed/kubectl-config
+cp -v test/test-configs/config.yaml  ~/.shed/config.yaml
 
 source /usr/lib/shed/shed-init
