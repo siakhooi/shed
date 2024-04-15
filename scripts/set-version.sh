5702c1f6de4f69992f997154f55879e0601d2819
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2024-04-15 03:22:50 +0000

    fix test expected output

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -5,3 +5,5 @@ set -e
 
 sed -i 'src/DEBIAN/control'  -e 's@Version: .*@Version: '$RELEASE_VERSION'@g'
 sed -i 'src/lib/shed-init'  -e 's@export SHED_VERSION=.*@export SHED_VERSION='$RELEASE_VERSION'@g'
+sed -i 'test/expected/shed-env/shed_env.out'  -e 's@SHED_VERSION=.*@SHED_VERSION='$RELEASE_VERSION'@g'
+sed -i 'test/expected/shed-env/shed-env.out'  -e 's@SHED_VERSION=.*@SHED_VERSION='$RELEASE_VERSION'@g'
