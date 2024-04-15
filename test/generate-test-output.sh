5a03ae572f85386d5a7fcf61f37d040d95108565
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2024-04-15 01:44:23 +0000

    remove redundant test

--- test/generate-test-output.sh
+++ test/generate-test-output.sh
@@ -7,9 +7,9 @@ fi
 OUTPUT_DIRECTORY=$1
 mkdir -p "$OUTPUT_DIRECTORY"
 
-./test/test-scripts/generate-test-output-shed-env.sh               "$OUTPUT_DIRECTORY/shed-env"
-./test/test-scripts/generate-test-output-shed-config.sh            "$OUTPUT_DIRECTORY/shed-config"
-./test/test-scripts/generate-test-output-shed-list.sh              "$OUTPUT_DIRECTORY/shed-list"
+#./test/test-scripts/generate-test-output-shed-env.sh               "$OUTPUT_DIRECTORY/shed-env"
+#./test/test-scripts/generate-test-output-shed-config.sh            "$OUTPUT_DIRECTORY/shed-config"
+#./test/test-scripts/generate-test-output-shed-list.sh              "$OUTPUT_DIRECTORY/shed-list"
 ./test/test-scripts/generate-test-output-shed-list-code.sh         "$OUTPUT_DIRECTORY/shed-list-code"
 ./test/test-scripts/generate-test-output-shed-values.sh            "$OUTPUT_DIRECTORY/shed-values"
 ./test/test-scripts/generate-test-output-shed-value-key-list.sh    "$OUTPUT_DIRECTORY/shed-value-key-list"
