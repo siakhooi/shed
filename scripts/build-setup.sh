0ea34e1f4351748f4c6fe30e83efb55b987817d8
Siak Hooi <siakhooi@gmail.com>
2025-02-25 11:41:12 +0000

    fix pandoc version

--- scripts/build-setup.sh
+++ scripts/build-setup.sh
@@ -2,8 +2,13 @@
 set -e
 set -x
 
-readonly pandoc_deb_url=https://github.com/jgm/pandoc/releases/download/3.6.3/pandoc-3.6.3-1-amd64.deb
-curl -LO "$pandoc_deb_url"
+tempDir=$(mktemp -d --tmpdir "$(basename "$0")-XXXXXXXXXX")
 
-sudo apt install -y ./pandoc-3.6.3-1-amd64.deb
+readonly pandoc_deb_url=https://github.com/jgm/pandoc/releases/download/3.6.3/pandoc-3.6.3-1-amd64.deb
+(
+  cd "$tempDir"
+  curl -LO "$pandoc_deb_url"
+  sudo apt install -y ./pandoc-3.6.3-1-amd64.deb
+)
+
 sudo apt install -y dpkg-dev
